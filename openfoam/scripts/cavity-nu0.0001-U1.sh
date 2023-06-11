858fcb68a15ea093cd5d5356a4627685b2774a2a
Harry McHugh <harrymchugh@gmail.com>
2023-06-11 14:50:40 +0200

    Add scripts for different reynolds numbers

---
+++ openfoam/scripts/cavity-nu0.0001-U1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /usr/lib/openfoam/openfoam2212/etc/bashrc
+cd /mnt/cases/cavity_nu0.0001_U1
+rm -r 0.* [1-9]*
+time blockMesh
+time icoFoam
+
+#Get number of CPUs available
+#lscpu | grep "^CPU(s):" | sed -e 's/\s\+/,/g' | cut -f 2 -d ","
+#
+#
--- openfoam/scripts/cavity-nu0.001-U1.sh
+++ openfoam/scripts/cavity-nu0.001-U1.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 source /usr/lib/openfoam/openfoam2212/etc/bashrc
-
-cd /mnt/cases
-
-for dir in cavity_nu*; do
-    cd $dir
-    rm -r 0.* [1-9]*
-    time blockMesh
-    time icoFoam
-    cd ../
-done
+cd /mnt/cases/cavity_nu0.001_U1
+rm -r 0.* [1-9]*
+time blockMesh
+time icoFoam
 
 #Get number of CPUs available
 #lscpu | grep "^CPU(s):" | sed -e 's/\s\+/,/g' | cut -f 2 -d ","
