c5fc47aacd3403430ad1f3824b0b1703ffe412f7
not populated <z2137380@cirrus-login2.ib0.icexa.epcc.ed.ac.uk>
2023-06-12 16:26:33 +0100

    Update inference script for Cirrus

--- inference/inference.sh
+++ inference/inference.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 #Macbook pro
-export VENV="$HOME/cfdpinn-venv/bin/activate" 
+#export VENV="$HOME/cfdpinn-venv/bin/activate" 
 #Work laptop
 #export VENV="$HOME/pinns-venv/bin/activate"
 #Cirrus
 #export VENV="$HOME/pinns-venv/bin/activate"
 
-export OMP_NUM_THREADS=$1
+export OMP_NUM_THREADS=1
 export OMP_PROC_BIND=True
 
 cfdpinn \
     --debug \
-    --profile \
-    --trace-path $HOME/pinns/profiles/100x100_0_5_0.001_trace.json \
-    --stack-path $HOME/pinns/profiles/100x100_0_5_0.001_stk.txt \
     --no-train \
     --case-type cavity \
     --case-dir $HOME/pinns/openfoam/cases/cavity_nu0.01_U1_100x100/ \
@@ -26,6 +23,6 @@ cfdpinn \
     --numy 100 \
     --viscosity 0.01 \
     --initial_u 1 \
-    --load-scaler-path $HOME/pinns/models/tmp-scaler.pkl \
-    --load-model-path $HOME/pinns/models/tmp-2d-ns-pinn-1epochs-lr0.001-adam-cavity-nu0.01-u1-20x20.pt
+    --load-scaler-path $WORK/pinns/models/scaler.pkl \
+    --load-model-path $WORK/pinns/models/2d-ns-pinn-1epochs-lr0.001-adam-cavity-nu0.01-u1-20x20.pt
     
