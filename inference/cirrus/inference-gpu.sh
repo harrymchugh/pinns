4e325477a4f0161eff022b0af11fc8b790a6a605
Harry McHugh <harrymchugh@gmail.com>
2023-06-12 20:56:22 +0200

    Update cirrus inference scripts to add profiling

--- inference/cirrus/inference-gpu.sh
+++ inference/cirrus/inference-gpu.sh
@@ -18,6 +18,9 @@ export OMP_PROC_BIND=True
 
 cfdpinn \
     --debug \
+    --profile \
+    --trace-path $CFDPINN_ROOT/profiles/100x100_0_5_0.001_GPU_trace.json \
+    --stack-path $CFDPINN_ROOT/profiles/100x100_0_5_0.001_GPU_stk.txt \
     --no-train \
     --case-type cavity \
     --case-dir $CFDPINN_ROOT/openfoam/cases/cavity_nu0.01_U1_100x100/ \
@@ -30,5 +33,5 @@ cfdpinn \
     --viscosity 0.01 \
     --initial_u 1 \
     --load-scaler-path $CFDPINN_ROOT/models/scaler.pkl \
-    --load-model-path $CFDPINN_ROOT/models/2d-ns-pinn-1epochs-lr0.001-adam-cavity-nu0.01-u1-20x20.pt
+    --load-model-path $CFDPINN_ROOT/models/tmp-2d-ns-pinn-1epochs-lr0.001-adam-cavity-nu0.01-u1-20x20.pt
     
--- inference/cirrus/inference.sh
+++ inference/cirrus/inference.sh
@@ -3,7 +3,7 @@
 #SBATCH --qos=standard
 #SBATCH --time=01:00:00
 #SBATCH --account=mdisspt-z2137380
-#SBATCH --job-name=inference-100x100
+#SBATCH --job-name=inference-100x100-cpu
 #SBATCH --output=%x.%j.out
 #SBATCH --exclusive
 
@@ -19,6 +19,9 @@ export OMP_PROC_BIND=True
 
 cfdpinn \
     --debug \
+    --profile \
+    --trace-path $CFDPINN_ROOT/profiles/100x100_0_5_0.001_CPU_trace.json \
+    --stack-path $CFDPINN_ROOT/profiles/100x100_0_5_0.001_CPU_stk.txt \
     --no-train \
     --case-type cavity \
     --case-dir $CFDPINN_ROOT/openfoam/cases/cavity_nu0.01_U1_100x100/ \
@@ -31,5 +34,5 @@ cfdpinn \
     --viscosity 0.01 \
     --initial_u 1 \
     --load-scaler-path $CFDPINN_ROOT/models/scaler.pkl \
-    --load-model-path $CFDPINN_ROOT/models/2d-ns-pinn-1epochs-lr0.001-adam-cavity-nu0.01-u1-20x20.pt
+    --load-model-path $CFDPINN_ROOT/models/tmp-2d-ns-pinn-1epochs-lr0.001-adam-cavity-nu0.01-u1-20x20.pt
     
