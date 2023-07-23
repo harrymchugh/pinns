4bca686ab446ad32dee6a0573517be9ebef36037
Harry McHugh <harrymchugh@gmail.com>
2023-07-23 15:01:20 +0100

    Tidy up tracking of models

--- openfoam/scripts/cavity-100x100.sh
+++ openfoam/scripts/cavity-100x100.sh
@@ -2,11 +2,10 @@
 
 source /usr/lib/openfoam/openfoam2212/etc/bashrc
 cd /mnt/cases/cavity_nu0.01_U1_100x100
+
+for i in 1 2 3 4 5; do
 rm -r 0.* [1-9]*
 time blockMesh
 time icoFoam
+done
 
-#Get number of CPUs available
-#lscpu | grep "^CPU(s):" | sed -e 's/\s\+/,/g' | cut -f 2 -d ","
-#
-#
